0a9c06ed773bfb1437b804828540c4ed196f0f53
cweaver <cweaver@FVFY52K9HV2H.local>
2019-06-03 23:54:17 -0400

    Add empty string test
    Method still passes!

--- string-calc.test.js
+++ string-calc.test.js
@@ -1,5 +1,9 @@
 const add = require('./string-calc');
 
-test('no input is given, should return 0', () => {
+test('given no input, should return 0', () => {
 	expect(add()).toBe(0);
+});
+
+test('given empty string, should return 0', () => {
+	expect(add("")).toBe(0);
 });
